abfa358c0675b540de59415872f565d9a4c903c3
Cvikli <havlikmarcell@gmail.com>
2024-05-13 14:28:43 +0200

    type problem resolved

--- src/Config.jl
+++ src/Config.jl
@@ -45,8 +45,8 @@ end
 set_day_range!(ctx; dayframe=0:42) = ctx.timestamps= ts2Δday(last(dayframe)):ts2Δday(first(dayframe))
 
 ts2Δday(ts)    = (Day(unix2datetime(ctx.now_ts)) - Day(unix2datetime(ts/1000))).value
-Δday2ts(day)   = floor(Int,datetime2unix(Δday2date(day)))
-Δday2date(day) = (global ctx; Day(unix2datetime(ctx.now_ts/1000))-Day(day))
+Δday2ts(day)   = cld(ctx.now_ts-day*60*60*24*1000, 60*60*24*1000) * 60*60*24 * 1000
+# Δday2date(day) = (global ctx; unix2datetime(ctx.now_ts/1000-day*60*60*24))
 
 
 
